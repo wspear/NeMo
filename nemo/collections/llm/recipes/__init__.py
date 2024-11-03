623d17a4e817a41aa500c3d8449ac0e58f4254aa
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-11-03 07:58:46 -0800

    use sgd in test_nemo_jit_cb
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo/collections/llm/recipes/__init__.py
+++ nemo/collections/llm/recipes/__init__.py
@@ -69,7 +69,7 @@ from nemo.collections.llm.recipes import (
     t5_220m,
 )
 from nemo.collections.llm.recipes.log.default import default_log, default_resume
-from nemo.collections.llm.recipes.optim import adam
+from nemo.collections.llm.recipes.optim import adam, sgd
 
 __all__ = [
     "baichuan2_7b",
@@ -126,6 +126,7 @@ __all__ = [
     "gemma2_9b",
     "gemma2_27b",
     "adam",
+    "sgd",
     "default_log",
     "default_resume",
 ]
